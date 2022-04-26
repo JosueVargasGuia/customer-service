bf822db565a5471761b41b69e1ddbdf3bd848e3c
JosueVargasGuia <44180784+JosueVargasGuia@users.noreply.github.com>
2022-04-26 13:09:38 -0500

    circuit Breaker

--- src/main/java/com/nttdata/customerservice/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/nttdata/customerservice/service/impl/CustomerServiceImpl.java
@@ -1,14 +1,10 @@
 package com.nttdata.customerservice.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Value; 
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
+import com.nttdata.customerservice.FeignClient.TableIdFeignClient;
 import com.nttdata.customerservice.entity.Customer;
 import com.nttdata.customerservice.repository.CustomerRepository;
 import com.nttdata.customerservice.service.CustomerService;
@@ -27,7 +23,8 @@ public class CustomerServiceImpl implements CustomerService {
 	
 	@Autowired
 	RestTemplate restTemplate;
-	
+	@Autowired
+	TableIdFeignClient tableIdFeignClient;
 	@Override 
 	public Flux<Customer> findAll() {
 		return repository.findAll();
@@ -43,6 +40,8 @@ public class CustomerServiceImpl implements CustomerService {
 				if(key>=1) {
 					customer.setId(key);
 					//log.info("SAVE[product]:"+customer.toString());
+				}else {
+					return Mono.error(new InterruptedException("Servicio no disponible:" + Customer.class.getSimpleName()));
 				}
 		return repository.save(customer);
 	}
@@ -58,7 +57,7 @@ public class CustomerServiceImpl implements CustomerService {
 	@Override
 	public Long generateKey(String nameTable) {
 		//log.info(tableIdService + "/generateKey/" + nameTable);
-		ResponseEntity<Long> responseGet = restTemplate.exchange(tableIdService + "/generateKey/" + nameTable, HttpMethod.GET,
+		/*ResponseEntity<Long> responseGet = restTemplate.exchange(tableIdService + "/generateKey/" + nameTable, HttpMethod.GET,
 				null, new ParameterizedTypeReference<Long>() {
 				});
 		if (responseGet.getStatusCode() == HttpStatus.OK) {
@@ -66,7 +65,8 @@ public class CustomerServiceImpl implements CustomerService {
 			return responseGet.getBody();
 		} else {
 			return Long.valueOf(0);
-		}
+		}*/
+		return tableIdFeignClient.generateKey(nameTable);
 	}
 
 }
